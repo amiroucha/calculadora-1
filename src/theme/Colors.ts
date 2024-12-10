112810876af871eea892e98ef729dc0f0c9b33c4
chaima <amirou.septiembre@gmail.com>
2024-12-10 19:05:41 +0100

    colores

--- src/theme/Colors.ts
+++ src/theme/Colors.ts
@@ -1,13 +1,11 @@
 export const Colors = {
-    green: '#3a6e3f',
-    grayLight: '#85737f',
-    violet: '#6e3968',
-    violetLight: '#ae448d',
+    grayLight: '#85737f',/*color operaciones*/
+    violet: '#6e3968',/*color operaciones extra*/
     textPrimary: 'white',
     textSecondary: '#666666',
     black: '#000',
     white : '#fff',
     blue:'#44597a', /*color numero*/
-    backColor: '#a79985' /*color claro*/
+    backColor: '#a79985' /*color claro fndoo*/
    /* #ccbca4*/
 }as const;
